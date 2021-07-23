c6c509e4dcfdc9f4d512064f25abcb43f68089ab
Bot8bit <82729927+Bot8bit@users.noreply.github.com>
2021-07-23 12:53:03 +0700

    Add files via upload

---
+++ uas.cpp
@@ -0,0 +1,411 @@
+#include<iostream>
+
+using namespace std;
+
+ main()
+{
+int a;
+char y ,t;
+cout<<"ALIF AINNUN QOYUM"<<endl;
+cout<<"191011450491"<<endl;
+cout<<"Anggi wibisono"<<endl;
+cout<<"191011450432"<<endl;
+cout<<"input 1 jika jawaban = salah"<<endl;
+cout<<"input 0 jika jawaban benar"<<endl;
+cout<<"==================================================="<<endl;
+cout<<" Apakah Sel batang retina sulit berdaptasi diruang yang remang-remang"<<endl;
+cout<<"input 0 jika jawaban benar"<<endl;
+cout<<"input 1 jika jawaban salah"<<endl;
+cin>>a;
+if(a ==0){
+	cout<<"Anda Berada rule 1"<<endl;
+    cout<<"1. Apakah Pada siang hari penglihatan menurun "<<endl;
+    cout<<"input 0 jika jawaban benar "<<endl;
+    cout<<"input 1 jika jawaban salah"<<endl;
+    cin>>a;
+     if(a == 0){
+        cout<<"Apakah Tidak dapat melihat pada lingkungan yang kurang bercahaya "<<endl;
+        cout<<"input 0"<<endl;
+        cin>>a;
+       if(a == 0){
+            cout<<"apakah Pada siang hari penglihatan menurun "<<endl;
+            cout<<"input 0"<<endl;
+            cin>>a;
+           if(a == 0){
+               cout<<"Xerophtalmania";
+
+					return 0; }}}}
+//no2//
+else if(a == 1){
+int a;
+char y ,t;
+cout<<"Anda Berada rule 2"<<endl;
+cout<<"Mata nyeri hebat "<<endl<<endl;
+cout<<"input 0 jika jawaban benar"<<endl;
+cout<<"input 1 jika jawaban salah"<<endl;
+cin>>a;
+if(a ==0){
+    cout<<"apakah Mata menonjol  dan Pergerakan mata terbatas "<<endl;
+    cout<<"input 0 jika jawaban benar "<<endl;
+    cin>>a;
+     if(a == 0){
+        cout<<"Apakah Kelopak Mata membengkak dan Mata tampak mengkilat   "<<endl;
+        cout<<"input 0"<<endl;
+        cin>>a;
+       if(a == 0){
+            cout<<"apakah Mata merah dan mata unggu "<<endl;
+            cout<<"input 0"<<endl;
+            cin>>a;
+           if(a == 0){
+               cout<<"Bola mata membengkak dan tampak berkabut "<<endl;
+               cout<<"jika iya anda mengalami Selulitis Orbitalitas ";
+
+				return 0; }}}}
+//no3//
+	else if(a == 1){
+int a;
+char y ,t;
+cout<<"Anda Berada rule 3"<<endl;
+cout<<"Apakah Sumber cahaya akan berwarna pelangi bila memandang lampu neon "<<endl<<endl;
+cout<<"input 0 jika jawaban benar"<<endl;
+cout<<"input 1 jika jawaban salah"<<endl;
+cin>>a;
+if(a ==0){
+    cout<<"Apakah Mata sakit "<<endl;
+    cout<<"input 0 jika jawaban benar "<<endl;
+    cin>>a;
+     if(a == 0){
+        cout<<"Apakah Mata membengkak  "<<endl;
+        cout<<"input 0"<<endl;
+        cin>>a;
+       if(a == 0){
+            cout<<"apakah Penglihatan yang tadinya kabur lama- kelamaan menjadi normal"<<endl;
+            cout<<"input 0"<<endl;
+            cin>>a;
+           if(a == 0){
+               cout<<"apakah biasa Menekan kedipan secara berlebihan  ";
+               if(a == 0){
+               cout<<"jika iya anda mengalami Glaukoma ";
+
+				return 0; }}}}}
+//no4//
+else if(a == 1){
+int a;
+char y ,t;
+cout<<"Anda Berada rule 4"<<endl;
+cout<<"Apakah air mata berlebihan "<<endl;
+cout<<"input 0 jika jawaban benar"<<endl;
+cout<<"input 1 jika jawaban salah"<<endl;
+cin>>a;
+if(a ==0){
+    cout<<"apakah Mata nyeri hebat "<<endl;
+    cout<<"input 0 jika jawaban benar "<<endl;
+    cin>>a;
+     if(a == 0){
+        cout<<"Apakah Mata membengkak   "<<endl;
+        cout<<"input 0"<<endl;
+        cin>>a;
+       if(a == 0){
+            cout<<"Anda mengalami sakit Dakriosistitis "<<endl;
+            return 0; }}}	
+//no5//
+else if(a == 1){
+int a;
+char y ,t;
+cout<<"Anda Berada rule 5"<<endl;
+cout<<"Apakah saat Malam hari kesulitan melihat "<<endl<<endl;
+cout<<"input 0 jika jawaban benar"<<endl;
+cin>>a;
+if(a ==0){
+    cout<<"Apakah pada siang hari penglihatan menurun  "<<endl;
+    cout<<"input 0 jika jawaban benar "<<endl;
+    cout<<"input 1 jika jawaban salah"<<endl;
+    cin>>a;
+     if(a == 0){
+        cout<<"Apakah Mata silau akan cahaya   "<<endl;
+        cout<<"input 0"<<endl;
+        cin>>a;
+       if(a == 0){
+            cout<<"Apakah Sering ganti kacamata  "<<endl;
+            cout<<"input 0"<<endl;
+            cin>>a;
+           if(a == 0){
+               cout<<"Apakah Mata nyeri hebat  "<<endl;
+               cout<<"input 0"<<endl;
+               if(a == 0){
+			   	cout<<"Lensa mata membengkak "<<endl;
+			   	cout<<"input 0"<<endl;
+			   	if(a == 0){
+				  cout<<"Anda mengalami sakit Katarak"<<endl; 
+
+				return 0; }}}}}}
+//no6//
+else if(a == 1){
+int a;
+char y ,t;
+cout<<"Anda Berada rule 6"<<endl;
+cout<<"Apakah Mata berair  "<<endl<<endl;
+cout<<"input 0 jika jawaban benar"<<endl;
+cout<<"input 1 jika jawaban salah"<<endl;
+cin>>a;
+if(a ==0){
+    cout<<"Apakah Mata nyeri hebat  "<<endl;
+    cout<<"input 0 jika jawaban benar "<<endl;
+    cin>>a;
+     if(a == 0){
+        cout<<"Apakah Mata gatal  "<<endl;
+        cout<<"input 0"<<endl;
+        cin>>a;
+       if(a == 0){
+            cout<<"Apakah Penglihatan kabur "<<endl;
+            cout<<"input 0"<<endl;
+            cin>>a;
+            if(a == 0){
+				cout<<"Apakah Peka terhadap cahaya  "<<endl;
+            	cout<<"input 0"<<endl;
+            	cin>>a;
+            	if(a == 0){
+            		cout<<"Apakah Berbentuk keropeng pada kelopak mata ketika bangun pada siang hari "<<endl;
+            		cout<<"input 0"<<endl;
+            		cin>>a;
+           			if(a == 0){
+               			cout<<"Anda mengalami sakit Konjungtivitis  ";
+               
+
+				return 0; }}}}}}
+//no7//
+else if(a == 1){
+int a;
+char y ,t;
+cout<<"Anda Berada rule 7"<<endl;
+cout<<"Apakah Kelopak Mata membengkak   "<<endl<<endl;
+cout<<"input 0 jika jawaban benar"<<endl;
+cin>>a;
+if(a ==0){
+    cout<<"Apakah Penglihatan menurun pada ruang gelap  "<<endl;
+    cout<<"input 0 jika jawaban benar "<<endl;
+    cout<<"input 1 jika jawaban salah"<<endl;
+    cin>>a;
+     if(a == 0){
+        cout<<"Apakah Penglihatan menurun pada malam hari   "<<endl;
+        cout<<"input 0"<<endl;
+        cin>>a;
+       if(a == 0){
+            cout<<"Anada mengalami penyakit Retinitis Pigmentosa"<<endl;
+            
+			return 0;}}}
+//no8//            
+else if(a == 1){
+int a;
+char y ,t;
+cout<<"Anda Berada rule 8"<<endl;
+cout<<"Apakah Kelopak Mata membengkak   "<<endl<<endl;
+cout<<"input 0 jika jawaban benar"<<endl;
+cin>>a;
+if(a ==0){
+    cout<<"Apakah Keluarnya cairan kotoran dari mata   "<<endl;
+    cout<<"input 0 jika jawaban benar "<<endl;
+    cin>>a;
+     if(a == 0){
+        cout<<"Apakah Berbaliknya bulu mata   "<<endl;
+        cout<<"input 0"<<endl;
+        cin>>a;
+        if(a == 0){
+        	cout<<"Apakah Pembengkakan kelenjar getah bening didepan telinga    "<<endl;
+        	cout<<"input 0"<<endl;
+        	cin>>a;
+        	if(a == 0){
+        		cout<<"Apakah Munculnya garis parutan pada kornea  "<<endl;
+        		cout<<"input 0"<<endl;
+        		cin>>a;
+        		if(a == 0){
+        			cout<<"Apakah Komplikasi pada,telinga,hidung dan tenggorokan  "<<endl;
+        			cout<<"input 0"<<endl;
+        			cin>>a;
+       				if(a == 0){
+            			cout<<"Anada mengalami penyakit Trakoma"<<endl;
+            
+			return 0;}}}}}}
+//no9//
+else if(a == 1){
+int a;
+char y ,t;
+cout<<"Anda Berada rule 9"<<endl;
+cout<<"Apakah Peka terhadap cahaya   "<<endl<<endl;
+cout<<"input 0 jika jawaban benar"<<endl;
+cout<<"input 1 jika jawaban salah"<<endl;
+cin>>a;
+if(a ==0){
+    cout<<"Apakah Mata berair   "<<endl;
+    cout<<"input 0 jika jawaban benar "<<endl;
+    cin>>a;
+    if(a == 0){
+        cout<<"Apakah Mata meradang  "<<endl;
+        cout<<"input 0"<<endl;
+        cin>>a;
+       	if(a == 0){
+            cout<<"Apakah Mata mempersempit, perubahan bentuk "<<endl;
+            cout<<"input 0"<<endl;
+            cin>>a;
+            if(a == 0){
+				cout<<"Apakah Benjolan pada mata bagian atas atau bawah"<<endl;
+            	cout<<"input 0"<<endl;
+            	cin>>a;
+            	if(a == 0){
+               			cout<<"Anda mengalami sakit Oveitis  ";
+				return 0; }}}}}
+//no10//	
+else if(a == 1){
+int a;
+char y ,t;
+cout<<"Anda Berada rule 10"<<endl;
+cout<<"Apakah Mata nyeri hebat   "<<endl<<endl;
+cout<<"input 0 jika jawaban benar"<<endl;
+cout<<"input 1 jika jawaban salah"<<endl;
+cin>>a;
+if(a ==0){
+    cout<<"Apakah Peka terhadap cahaya "<<endl;
+    cout<<"input 0 jika jawaban benar "<<endl;
+    cin>>a;
+    if(a == 0){
+        cout<<"Apakah Mata merah   "<<endl;
+        cout<<"input 0"<<endl;
+        cin>>a;
+       	if(a == 0){
+            cout<<"Apakah Mata Berair "<<endl;
+            cout<<"input 0"<<endl;
+            cin>>a;
+            if(a == 0){
+               			cout<<"Anda mengalami sakit Hordeolum ";
+				return 0; }}}}							
+//rule11//
+else if(a == 1){
+int a;
+char y ,t;
+cout<<"Anda Berada rule 11"<<endl;
+cout<<"Apakah Gangguan penglihatan pada salah satu mata "<<endl<<endl;
+cout<<"input 0 jika jawaban benar"<<endl;
+cout<<"input 1 jika jawaban salah"<<endl;
+cin>>a;
+if(a ==0){
+    cout<<"Apakah Garis mata lurus terlihat bergelombang  "<<endl;
+    cout<<"input 0 jika jawaban benar "<<endl;
+    cin>>a;
+    if(a == 0){
+        cout<<"Apakah Mata tidak nyeri    "<<endl;
+        cout<<"input 0"<<endl;
+        cin>>a;
+       	if(a == 0){
+            cout<<"Anda mengalami sakit Degenerasi Makula";
+		return 0; }}}	
+//rule12//				
+else if(a == 1){
+int a;
+char y ,t;
+cout<<"Anda Berada rule 12"<<endl;
+cout<<"Apakah Penglihatan kabur   "<<endl<<endl;
+cout<<"input 0 jika jawaban benar"<<endl;
+cout<<"input 1 jika jawaban salah"<<endl;
+cin>>a;
+if(a ==0){
+    cout<<"Apakah Mata tegang  "<<endl;
+    cout<<"input 0 jika jawaban benar "<<endl;
+    cin>>a;
+    if(a == 0){
+        cout<<"Apakah Mata melihat melayang-layang  "<<endl;
+        cout<<"input 0"<<endl;
+        cin>>a;
+       	if(a == 0){
+            cout<<"Apakah Mata melihat kilatan cahaya  "<<endl;
+            cout<<"input 0"<<endl;
+            cin>>a;
+            if(a == 0){
+               			cout<<"Anda mengalami sakit Ablasio Retina";
+				return 0; }}}}					
+//rule13//
+else if(a == 1){
+int a;
+char y ,t;
+cout<<"Anda Berada rule 13"<<endl;
+cout<<"Apakah Penglihatan kabur  "<<endl<<endl;
+cout<<"input 0 jika jawaban benar"<<endl;
+cout<<"input 1 jika jawaban salah"<<endl;
+cin>>a;
+if(a ==0){
+    cout<<"Apakah Mata merah  "<<endl;
+    cout<<"input 0 jika jawaban benar "<<endl;
+    cin>>a;
+     if(a == 0){
+        cout<<"Apakah Mata berair  "<<endl;
+        cout<<"input 0"<<endl;
+        cin>>a;
+        if(a == 0){
+        	cout<<"Apakah Mata kering dan iritasi "<<endl;
+        	cout<<"input 0"<<endl;
+        	cin>>a;
+        	if(a == 0){
+        		cout<<"Apakah Benjolan pada mata bagian atas atau bawah  "<<endl;
+        		cout<<"input 0"<<endl;
+        		cin>>a;
+        		if(a == 0){
+        			cout<<"Apakah Seperti ada benda asing di mata  "<<endl;
+        			cout<<"input 0"<<endl;
+        			cin>>a;
+       				if(a == 0){
+            			cout<<"Anada mengalami penyakit Pterygium"<<endl;
+            
+			return 0;}}}}}}
+//rule14//
+else if(a == 1){
+int a;
+char y ,t;
+cout<<"Anda Berada rule 14"<<endl;
+cout<<"Apakah Mata nyeri hebat  "<<endl<<endl;
+cout<<"input 0 jika jawaban benar"<<endl;
+cout<<"input 1 jika jawaban salah"<<endl;
+cin>>a;
+if(a ==0){
+    cout<<"Apakah Mata perih  "<<endl;
+    cout<<"input 0 jika jawaban benar "<<endl;
+    cin>>a;
+    if(a == 0){
+        cout<<"Apakah Air mata berlebihan  "<<endl;
+        cout<<"input 0"<<endl;
+        cin>>a;
+       	if(a == 0){
+       		cout<<"Apakah sakit kepala  "<<endl;
+        cout<<"input 0"<<endl;
+        cin>>a;
+       	if(a == 0){
+            cout<<"Anda mengalami sakit Miopi";
+				return 0; }}}}
+//rule15//
+else if(a == 1){
+int a;
+char y ,t;
+cout<<"Anda Berada rule 15"<<endl;
+cout<<"Apakah Penglihatan kabur "<<endl<<endl;
+cout<<"input 0 jika jawaban benar"<<endl;
+cout<<"input 1 jika jawaban salah"<<endl;
+cin>>a;
+if(a ==0){
+    cout<<"Apakah Mata merah "<<endl;
+    cout<<"input 0 jika jawaban benar "<<endl;
+    cin>>a;
+    if(a == 0){
+        cout<<"Apakah Kelopak Mata membengkak  "<<endl;
+        cout<<"input 0"<<endl;
+        cin>>a;
+       	if(a == 0){
+            cout<<"Apakah Mata nyeri bila ditekan  "<<endl;
+            cout<<"input 0"<<endl;
+            cin>>a;
+            if(a == 0){
+            	cout<<"Apakah Riwayat penyakit menular seksual pada ibu  "<<endl;
+            	cout<<"input 0"<<endl;
+            	cin>>a;
+            	if(a == 0){
+               		cout<<"Anda mengalami sakit Oftalmia Neonatorium";
+				return 0; }}}}				
+}}}}}}}}
+}}}}}}}}
